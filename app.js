a8463e3c3a1918ec30d3b97b7138879ebf63f89c
Garvit Jain <jain.garvit7@gmail.com>
2019-01-30 02:58:41 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 var express     = require('express');
 var app         = express();
+var i           = 0;
 
-const server = require('http').createServer();
-var io          = require("socket.io")(server);
+var server     = app.listen(3000);
+var io          = require("socket.io").listen(server);
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
 
 io.on("connection",(socket) =>{
-   console.log("New user connected"); 
+   i=i+1;
+   console.log("New User Connected who is number :- " + i + " .");
+   
+   socket.username = "Anonymous";
+
+   socket.on('change_username', function(data){
+      console.log("New Username :- "+ data.username);
+      socket.username = data.username;
+   });
+
+   socket.on('new_message', function(data){
+      io.sockets.emit('new_message', {message: data.message, username: socket.username});
+   });
 });
 
 app.get("/",function(req,res){
    res.render("index");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(5002, function(){
     console.log("The Server has been started");
 });
--- public/index.js
+++ public/index.js
@@ -1,3 +1,28 @@
+var i= 0;
 $(function(){
-   var socket = io.connect("https://chat-app-gj1197.c9users.io");
+
+   var socket = io.connect("localhost:3000");
+
+   var message = $("#msg");
+   var username = $("#chuser_field");
+   var send_username = $("#chuserbutton");
+   var send_message = $("#msg_send");
+   var chatroom = $("#chat");
+
+   send_username.click(function(){
+      console.log("New username :- "+ username.val());
+      socket.emit('change_username', {username: username.val()});
+   });
+   send_message.click(function(){
+      i=i+1;
+      console.log("This is " + i + " th message and it is :- '" + message.val() + "'");
+      socket.emit("new_message", {message: message.val()});
+   });
+
+   socket.on("new_message", function(data){
+      console.log("Username :- "+ data.username);
+      console.log("Message :- " + data.message);
+      chatroom.prepend("<div class='messages'><span class = 'username_box'>"+ data.username + "</span><span class='message_data'>"+ data.message + "</span></div>");
+   });
 });
+
